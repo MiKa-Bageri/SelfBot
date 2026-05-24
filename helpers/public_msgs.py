04cee48ef5300465205983743205195db8ecfd49
Milaadmg023 <bagerim962@gmail.com>
2026-05-24 08:58:24 +0330

    fix: update customer retrieval to async

--- helpers/public_msgs.py
+++ helpers/public_msgs.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-from modules.db import DB_Controller
+from models.db import DB_Controller
 import config
 import asyncio
 
@@ -13,7 +13,7 @@ chat_id = config.CHANNEL
 msg_id = config.PUBLIC_MSG
 
 async def public_msgs(client: Client, message: Message):
-    customers = db.get_customers()
+    customers = await db.get_customers()
     for user_id in customers:
         try:
             await client.copy_message(chat_id=user_id, from_chat_id=chat_id, message_id=msg_id)
